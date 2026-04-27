1454e1411d67926b5a46a2908beb1db7b61486bc
Rohit Kumar <114604560+allrd@users.noreply.github.com>
2026-04-27 14:00:37 +0530

    Include 'columns_in_file' in response data
    Added missing 'columns_in_file' key to response data.

--- app.py
+++ app.py
@@ -158,9 +158,9 @@ def process_daily_expense_excel(path, emp, ctype, voucher, db_df, c_id, voucherN
                 "data": {
                     "claim_id": c_id,
                     "voucher_number":voucherNumber,
-                    "Details":df.columns.tolist()
-                }
-                
+                    "columns_in_file":df.columns.tolist()
+                },
+                "errors": []
             }
             # return {"code":1,
             #         "claim_id":c_id,
